b13b99f23854b6a9b4eac3b5d0d12e4cd68cf4aa
jasonbanboa <jasonbanboa123@gmail.com>
2023-04-24 23:51:11 +0900

    styled for main page and refactored fetching data

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,14 @@ import CountryDiv from './components/CountryDiv'
 
 function App() {
   const [countries, setCountries] = useState<Country[]>([]);
-
+  
+  const fetchCountries = async () => {
+    const url = '/src/countries/data.json';
+    const DATA = await fetch(url).then(res => res.json());
+    setCountries(DATA);
+  }
+  
   useEffect( () => {
-    const fetchCountries = async () => {
-      const url = '/src/countries/data.json';
-      const DATA = await fetch(url).then(res => res.json());
-      setCountries(DATA);
-    }
     fetchCountries();
   }, []);
   
@@ -21,12 +22,13 @@ function App() {
   }, [countries]);
 
   return (
-    <>
-      {countries.map((country, i) => {
-        return <CountryDiv key={i} country={country} />
-      })
-      }
-    </>
+    <div id="app" className="light-theme">
+      <main>
+        {countries.slice(0, 20).map((country, i) => {
+          return <CountryDiv key={i} country={country} />
+        })}
+      </main>
+    </div>
   )
 }
 
--- src/components/CountryDiv.tsx
+++ src/components/CountryDiv.tsx
@@ -6,11 +6,18 @@ interface CountryDivProps {
   country: Country;
 }
 
-const CountryDiv: React.FC<CountryDivProps> = ({ country: { name, population, flag } }) => {
+const CountryDiv: React.FC<CountryDivProps> = ({ country: { name, population, flag, region, capital } }) => {
   return (
     <article className="indi-country">
-      <img src={flag} alt={name} />
-      {`${name}  ${population}`}
+      <div className="img-container">
+        <img src={flag} alt={name} />
+      </div>
+      <div className="country-info">
+        <h3 className="f-800">{name}</h3>
+        <p className="f-600">Population: <span className="f-300">{population.toLocaleString()}</span></p>
+        <p className="f-600">Region: <span className="f-300">{region}</span></p>
+        <p className="f-600">Capital: <span className="f-300">{capital}</span></p>
+      </div>
     </article>
   );
 }
