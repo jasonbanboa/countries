c8188a6cd928c895951a3672947f9f20a80e41ea
jasonbanboa <jasonbanboa123@gmail.com>
2023-04-26 14:11:00 +0900

    feat header with dark and light theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react"
 import { Country } from "./countriesService"
-import CountryDiv from './components/CountryDiv' 
+import CountryDiv from "./components/CountryDiv" 
 import FilterSearch from "./components/FilterSearch"
+import Header from "./components/Header"
 
 function App() {
+  const [darkTheme, setDarkTheme] = useState(false);
   const [countries, setCountries] = useState<Country[]>([]);
   
   const fetchCountries = async () => {
@@ -18,11 +20,11 @@ function App() {
   
   useEffect(() => {
     console.log(countries);
-    
   }, [countries]);
 
   return (
-    <div id="app" className="light-theme">
+    <div id="app" className={darkTheme ? "dark-theme" : "light-theme"}>
+      <Header darkTheme={darkTheme} setDarkTheme={setDarkTheme}/>
       <FilterSearch />
       <main>
         {countries.slice(0, 20).map((country, i) => {
