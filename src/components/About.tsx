c3ce1cbaa2fae0b8aaa6b1c438e37c521ef686af
jasonbanboa <jasonbanboa123@gmail.com>
2023-04-27 21:00:28 +0900

    styled about page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,8 +1,8 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { useParams, Link } from 'react-router-dom'
-import { ThemeContext } from '../context/Theme';
-import fetchCountries, { Country } from '../countriesService';
-import Header from './Header';
+import { ThemeContext } from '../context/Theme'
+import fetchCountries, { Country } from '../countriesService'
+import Header from './Header'
 
 const About: React.FC = () => {
   const [countries, setCountries] = useState<Country[]>([]);
@@ -36,25 +36,41 @@ const About: React.FC = () => {
             </button>
           </Link>
         </div>
-        <article>
-          <div className="img-container">
+        <article className="flex about-main">
+
+          <div className="img-container flex-1">
             <img src={country?.flag} alt="" />
           </div>
-          <h3>{country?.name}</h3>
-          <div className="f-800">Native Name: <span className="f-300">{country?.nativeName}</span></div>
-          <div className="f-800">Population: <span className="f-300">{country?.population}</span></div>
-          <div className="f-800">Region: <span className="f-300">{country?.region}</span></div>
-          <div className="f-800">Sub Region: <span className="f-300">{country?.subregion}</span></div>
-          <div className="f-800">Capital: <span className="f-300">{country?.capital || 'No Capital'}</span></div>
-          <div className="f-800">Top Level Domain: <span className="f-300">{country?.topLevelDomain || 'No top level Doamin'}</span></div>
-          <div className="f-800">Currencies: <span className="f-300">{countryCurrency || 'No Currency'}</span></div>
-          <div className="f-800">Languages: <span className="f-300">{countryLanguages || 'No Languages'}</span></div>
-          <div className="f-800">Border Countries: 
-            <span className="f-300">{countryBordersArr  
-            ? countryBordersArr.map(({ name }) => {
-              return <span>{name}</span>;
-            }) :'No Bordering Countries'}
-            </span>
+
+          <div className="flex-1 text-section flex">
+            <h3 className="country-name-h3">{country?.name}</h3>
+            
+            <div className="wrapper country-data flex">
+
+              <div className="inner-wrapper flex-1">
+                <div className="f-800">Native Name: <span className="f-300">{country?.nativeName}</span></div>
+                <div className="f-800">Population: <span className="f-300">{country?.population}</span></div>
+                <div className="f-800">Region: <span className="f-300">{country?.region}</span></div>
+                <div className="f-800">Sub Region: <span className="f-300">{country?.subregion}</span></div>
+                <div className="f-800">Capital: <span className="f-300">{country?.capital || 'No Capital'}</span></div>
+              </div>  
+                
+              <div className="inner-wrapper flex-1">
+                <div className="f-800">Top Level Domain: <span className="f-300">{country?.topLevelDomain || 'No top level Doamin'}</span></div>
+                <div className="f-800">Currencies: <span className="f-300">{countryCurrency || 'No Currency'}</span></div>
+                <div className="f-800">Languages: <span className="f-300">{countryLanguages || 'No Languages'}</span></div>
+              </div>
+
+            </div>
+
+            <div className="f-800 border-countries">Border Countries: 
+              <span className="f-300 flex gap-1 flex-wrap" style={{paddingTop: '10px'}}>{countryBordersArr  
+              ? countryBordersArr.map(({ name }) => {
+                return <span className="bordering-country-link"><Link to={`/about/${name}`}>{name}</Link></span>;
+              }) :'No Bordering Countries'}
+              </span>
+            </div>
+
           </div>
         </article>
       </section>
